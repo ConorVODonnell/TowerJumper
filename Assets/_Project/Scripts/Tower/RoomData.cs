978f9942655c2ee042e1ba576428cb044c693acb
Conor <67388650+ConorVODonnell@users.noreply.github.com>
2022-05-24 14:19:02 -0400

    Reorganizing files.

---
+++ Assets/_Project/Scripts/Tower/RoomData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Room Type")]
+public class RoomData : ScriptableObject
+{
+    public int numOfEnemies;
+}
+ 
